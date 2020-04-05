78b77a7dcf40c13e2d8b6893940d0b1b7c2a7569
DevRappers <devrappers@gmail.com>
2020-04-05 20:13:22 +0900

    useCallback 최적화, _app.js를 통한 layout분리

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,20 +1,9 @@
 import React from "react";
-import Head from "next/head";
-import AppLayout from "../components/AppLayout";
 
 const Profile = () => {
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.1.0/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <div>Profile</div>
-      </AppLayout>
+      <div>Profile</div>
     </>
   );
 };
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
-import Head from "next/head";
+import React, { useState, useCallback } from "react";
 import { Input, Checkbox, Button } from "antd";
-import AppLayout from "../components/AppLayout";
 
 const SignUp = () => {
   const useInput = (initValue = null) => {
     const [value, setter] = useState(initValue);
-    const handler = e => {
+    const handler = useCallback(e => {
       setter(e.target.value);
-    };
+    }, []);
     return [value, handler];
   };
 
@@ -20,93 +18,90 @@ const SignUp = () => {
   const [passwordError, setPasswordError] = useState(false);
   const [termError, setTermError] = useState(false);
 
-  const onSubmit = e => {
-    e.preventDefault();
-    if (password !== passwordCheck) {
-      return setPasswordError(true);
-    }
-    if (!term) {
-      return setTermError(true);
-    }
-    console.log({ id, nick, password, passwordCheck, term });
-  };
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      if (password !== passwordCheck) {
+        return setPasswordError(true);
+      }
+      if (!term) {
+        return setTermError(true);
+      }
+      console.log({ id, nick, password, passwordCheck, term });
+    },
+    [password, passwordCheck, term]
+  );
 
-  const onChangePasswordChk = e => {
-    setPasswordError(e.target.value !== password);
-    setPasswordCheck(e.target.value);
-  };
+  const onChangePasswordChk = useCallback(
+    e => {
+      setPasswordError(e.target.value !== password);
+      setPasswordCheck(e.target.value);
+    },
+    [password]
+  );
 
-  const onChangeTerm = e => {
+  const onChangeTerm = useCallback(e => {
     setTermError(false);
     setTerm(e.target.checked);
-  };
+  }, []);
 
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.1.0/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <form onSubmit={onSubmit} style={{ padding: 10 }}>
-          <div>
-            <label htmlFor="user-id">아이디</label>
-            <br />
-            <Input name="user-id" value={id} required onChange={onChangeId} />
-          </div>
-          <div>
-            <label htmlFor="user-nick">닉네임</label>
-            <br />
-            <Input
-              name="user-nick"
-              value={nick}
-              required
-              onChange={onChangeNick}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">비밀번호</label>
-            <br />
-            <Input
-              name="user-password"
-              type="password"
-              value={password}
-              required
-              onChange={onChangePassword}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password-chk">비밀번호 체크</label>
-            <br />
-            <Input
-              name="user-password-chk"
-              type="password"
-              value={passwordCheck}
-              required
-              onChange={onChangePasswordChk}
-            />
-            {passwordError && (
-              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
-            )}
-          </div>
-          <div>
-            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
-              동의
-            </Checkbox>
-            {termError && (
-              <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
-            )}
-          </div>
-          <div style={{ marginTop: 10 }}>
-            <Button type="primary" htmlType="submit">
-              가입하기
-            </Button>
-          </div>
-        </form>
-      </AppLayout>
+      <form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick"
+            value={nick}
+            required
+            onChange={onChangeNick}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-chk">비밀번호 체크</label>
+          <br />
+          <Input
+            name="user-password-chk"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordChk}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            동의
+          </Checkbox>
+          {termError && (
+            <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
+          )}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </form>
     </>
   );
 };
