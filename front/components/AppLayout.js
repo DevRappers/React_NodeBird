dd4f779b0f207394bda4e26a0fa9665b4c8132da
DevRappers <devrappers@gmail.com>
2020-04-05 20:46:46 +0900

    antd grid

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu, Input, Button } from "antd";
+import { Menu, Input, Button, Row, Col, Card, Avatar } from "antd";
+
+const dummy = {
+  nickname: "DevRappers",
+  Post: [],
+  Followings: [],
+  Followers: []
+};
 
 const AppLayout = ({ children }) => {
   return (
@@ -26,7 +33,38 @@ const AppLayout = ({ children }) => {
           <Button>SignUp</Button>
         </a>
       </Link>
-      {children}
+      <Row>
+        <Col xs={24} md={6}>
+          <Card
+            actions={[
+              <div key="twit">
+                짹짹
+                <br />
+                {dummy.Post.length}
+              </div>,
+              <div key="following">
+                팔로잉
+                <br />
+                {dummy.Followings.length}
+              </div>,
+              <div key="follower">
+                팔로워
+                <br />
+                {dummy.Followers.length}
+              </div>
+            ]}
+          >
+            <Card.Meta
+              avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+              title={dummy.nickname}
+            />
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}></Col>
+      </Row>
     </div>
   );
 };
