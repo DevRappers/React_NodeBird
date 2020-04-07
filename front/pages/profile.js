1ceb2783615d6056130cfa28ffdbb692164017e3
DevRappers <devrappers@gmail.com>
2020-04-07 20:57:52 +0900

    Profile Page

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,10 +1,53 @@
 import React from "react";
+import { Input, Button, List, Card } from "antd";
+import { StopOutlined } from "@ant-design/icons";
 
 const Profile = () => {
   return (
-    <>
-      <div>Profile</div>
-    </>
+    <div>
+      <form
+        style={{
+          marginBottom: "20px",
+          border: "1px solid #d9d9d9",
+          padding: "20px"
+        }}
+      >
+        <Input addonBefore="닉네임" />
+        <Button type="primary">수정</Button>
+      </form>
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>팔로잉 목록</div>}
+        loadMore={<Button style={{ width: "100%" }}>더 보기</Button>}
+        bordered
+        dataSource={["DevRappers", "이재준", "asapz"]}
+        renderItem={item => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<StopOutlined />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>팔로워 목록</div>}
+        loadMore={<Button style={{ width: "100%" }}>더 보기</Button>}
+        bordered
+        dataSource={["DevRappers", "이재준", "asapz"]}
+        renderItem={item => (
+          <List.Item style={{ marginTop: "20px" }}>
+            <Card actions={[<StopOutlined />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
   );
 };
 
